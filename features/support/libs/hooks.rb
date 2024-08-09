4c764dd7f06883a02d4dce865e796512dc39c70d
PedroCarniel <pedrohcastamancarniel@gmail.com>
2024-08-08 21:34:50 -0300

    Ajustes/Modificações

--- features/support/libs/hooks.rb
+++ features/support/libs/hooks.rb
@@ -4,12 +4,12 @@ require 'mongo'
 MongoClient = Mongo::Client.new(['127.0.0.1:27017'], database: 'rocklov')
 
 at_exit do
-  # Conecte-se ao cliente MongoDB
+  # Loga no banco
   client = MongoClient
 
-  # Drope o banco de dados
+  # Dropa o banco de dados
   client.database.drop
 
-  # Feche a conexão
+  # Fecha a conexão
   client.close
 end
